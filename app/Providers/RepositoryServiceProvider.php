6cd344c7bf1608f2f2d916ea441a2d5cbf3c0a50
abderrahmane140 <bsarabderrahmane01@gmail.com>
2026-03-28 23:59:25 +0100

    add the enrollment and grlup repository to service provider

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -1,36 +1,40 @@
 <?php
 
+
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use App\Repositories\Interfaces\UserRepositoryInterface;
 use App\Repositories\UserRepository;
-
-// use App\Repositories\Interfaces\CourseRepositoryInterface;
-// use App\Repositories\CourseRepository;
-// use App\Repositories\Interfaces\EnrollmentRepositoryInterface;
-// use App\Repositories\EnrollmentRepository;
-// use App\Repositories\Interfaces\GroupRepositoryInterface;
-// use App\Repositories\GroupRepository;
-// use Illuminate\Contracts\Auth\UserProvider;
+use App\Repositories\Interfaces\CourseRepositoryInterface;
+use App\Repositories\CourseRepository;
+use App\Repositories\Interfaces\EnrollmentRepositoryInterface;
+use App\Repositories\EnrollmentRepository;
+use App\Repositories\Interfaces\GroupRepositoryInterface;
+use App\Repositories\GroupRepository;
+use App\Models\User;
+use App\Models\Course;
+use App\Models\Enrollment;
+use App\Models\Group;
 
 class RepositoryServiceProvider extends ServiceProvider
 {
-    /**
-     * Register services.
-     */
     public function register(): void
     {
-        $this->app->bind(UserRepositoryInterface::class, UserRepository::class);        // $this->app->bind(CourseRepositoryInterface::class, CourseRepository::class);
-        // $this->app->bind(EnrollmentRepositoryInterface::class, EnrollmentRepository::class);
-        // $this->app->bind(GroupRepositoryInterface::class, GroupRepository::class);
-    }
+        $this->app->bind(UserRepositoryInterface::class, function ($app) {
+            return new UserRepository(new User());
+        });
 
-    /**
-     * Bootstrap services.
-     */
-    public function boot(): void
-    {
-        //
+        $this->app->bind(CourseRepositoryInterface::class, function () {
+            return new CourseRepository(new Course());
+        });
+
+        $this->app->bind(EnrollmentRepositoryInterface::class, function () {
+            return new EnrollmentRepository(new Enrollment());
+        });
+
+        $this->app->bind(GroupRepositoryInterface::class, function () {
+            return new GroupRepository(new Group());
+        });
     }
 }
