b486afd549391b8f85f6cee35b09d738a0f469f9
abderrahmane140 <bsarabderrahmane01@gmail.com>
2026-03-29 00:01:05 +0100

    implement the jwt subject on the user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,8 +7,9 @@ use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use PHPOpenSourceSaver\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject 
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
